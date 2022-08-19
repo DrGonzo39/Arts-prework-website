edfa7d363a5e0c870d766343ddda02e5fcb06b7c
Arthur Benware <artpolitic91@gmail.com>
2022-08-18 20:20:33 -0400

    changed javascript text,with plan to function

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content created by JavaScript"
+h2.textContent = "Since this text is out of place, i'm gonna make it do something cool"
 document.querySelector("body").appendChild(h2);
