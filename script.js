c25e40ae482b0615508e1ad6157c4fbbd54a40f2
Arthur Benware <artpolitic91@gmail.com>
2022-08-18 20:58:08 -0400

    added mouseover event to javascript text

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Since this text is out of place, i'm gonna make it do something cool"
 document.querySelector("body").appendChild(h2);
+const h22 = document.querySelector("h2");
+h22.addEventListener('mouseover', function handleMouseOver () {
+    h22.style.color = 'green';
+});
+h22.addEventListener('mouseout', function handleMouseOut() {
+    h22.style.color = 'black';
+});
